8329c1111fb9513da1092032043e9438be104a3a
CPPPrimerEnthusiast <alexandermmail@gmail.com>
2016-03-25 11:39:49 +0100

    Update ex14_38_39.cpp
    Shorter solution.

--- ch14/ex14_38_39.cpp
+++ ch14/ex14_38_39.cpp
@@ -1,69 +1,28 @@
-//
-// Exercise 14.38:
-// Write a class that tests whether the length of a given string matches a
-// given bound. Use that object to write a program to report how many words
-// in an input file are of sizes 1 through 10 inclusive.
-//
-// Exercise 14.39:
-// Revise the previous program to report the count of words that are sizes
-// 1 through 9 and 10 or more.
-//
-
 #include <iostream>
-#include <string>
 #include <fstream>
-#include <vector>
-#include <map>
 
-struct IsInRange
-{
-    IsInRange(std::size_t lower, std::size_t upper)
-        :_lower(lower), _upper(upper)
-    { }
-
-    bool operator()(std::string const& str) const
-    {
-        return str.size() >= _lower && str.size() <= _upper;
-    }
-
-    std::size_t lower_limit() const
-    {
-        return _lower;
-    }
-
-    std::size_t upper_limit() const
-    {
-        return _upper;
-    }
+struct bound {
+    
+    bound(const size_t&b, const size_t&e): begin(b), end(e) {}
+    
+    // Bounded interval.
+    bool operator()(const std::string&s) { return s.size() >= begin && s.size() <= end; }
+    
 private:
-    std::size_t _lower;
-    std::size_t _upper;
+    const size_t begin, end;
 };
 
-int main()
-{
-    //create predicates with various upper limits.
-    std::size_t lower = 1;
-    auto uppers = { 3u, 4u, 5u, 6u, 7u, 8u, 9u, 10u, 11u, 12u, 13u, 14u };
-    std::vector<IsInRange> predicates;
-    for (auto upper : uppers)
-        predicates.push_back(IsInRange{ lower, upper });
-
-    //create count_table to store counts 
-    std::map<std::size_t, std::size_t> count_table;
-    for (auto upper : uppers)
-        count_table[upper] = 0;
-
-    //read file and count
-    std::ifstream fin("../data/storyDataFile.txt");
-    for (std::string word; fin >> word; /* */)
-        for (auto is_size_in_range : predicates)
-            if (is_size_in_range(word))
-                ++count_table[is_size_in_range.upper_limit()];
-
-    //print
-    for (auto pair : count_table)
-        std::cout << "count in range [1, " << pair.first << "] : " << pair.second << std::endl;
-
-    return 0;
+int main(int argc, const char *argv[]) {
+    
+    if(argc != 2) { 
+        std::cerr << "You must specify a file path in order to use this program." << std::endl; 
+        return EXIT_FAILURE; 
+    }
+    
+    std::ifstream file(argv[1]);
+    std::istream_iterator<std::string> beg(file), end;
+    
+    auto count = std::count_if(beg, end, bound(1, 10));
+    
+    std::cout << "Number of elements of sizes between 1-10: " << count << std::endl;
 }
